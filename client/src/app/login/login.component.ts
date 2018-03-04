b05768e735b829ab2dfbb5298b24abd09ad89f5e
kotar0 <koutaro.okuya@gmail.com>
2018-03-04 22:43:21 +0900

    loginComponent フォーム入力内容取得機能追加

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../models/Login';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    username = new FormControl();
+    password = new FormControl();
+    loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+     private _fb: FormBuilder
+  ) { }
+
 
   ngOnInit() {
+      this.initForm();
+  }
+
+  initForm(): void {
+    this.loginForm = this._fb.group({
+        'password': this.password,
+        'user_name': this.username
+    });
+  }
+
+  onSubmitLogin(loginData: Login): void {
+    console.log(loginData);
   }
 
 }
