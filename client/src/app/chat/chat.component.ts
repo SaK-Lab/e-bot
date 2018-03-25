5ccc11a5188105e2bf6ce75e97bd5edd8e3124e0
kotar0 <koutaro.okuya@gmail.com>
2018-03-26 06:58:53 +0900

    ADD: change chat component

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { ThinkService } from '../thinklist/think.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+
+// Models
 import { ThinkResult } from '../models/ThinkResult';
+import { MsgItem } from '../models/MsgItem';
+
+
+
+
 
 
 @Component({
@@ -8,25 +17,39 @@ import { ThinkResult } from '../models/ThinkResult';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit {
 
   counter = 0;
-  thinkArray: [ThinkResult];
+  thinkArray: MsgItem[];
   currentMsg: string;
   currentMsgId: number;
 
+  @ViewChild('btn') btn: ElementRef;
+
   constructor(private _thinkService: ThinkService) { }
+
+  ngAfterViewInit() {
+    const btn$ = Observable.fromEvent(
+      this.btn.nativeElement, 'click'
+    ).subscribe(
+      succ => {
+        console.log('EventEmit');
+      },
+      err => console.log(err),
+      () => console.log('done')
+    );
+  }
 
   ngOnInit() {
     console.log(this.counter);
     this._thinkService.getThinks()
       .subscribe(
         succ => {
-          this.thinkArray = succ['data'];
+          console.log(succ);
+          this.thinkArray = succ;
           this.currentMsg = this.thinkArray[this.counter]['content'];
           this.currentMsgId = this.thinkArray[this.counter]['id'];
           console.log(this.thinkArray);
-
         },
         err => console.log(err),
         () => console.log('done')
